948221fe743fe37d7b71877f2c31c1a8d4d06120
fullstack251022 <fullstack251022@gmail.com>
2023-02-21 21:00:58 +0200

    changed knex.js to use process.env

--- database/knex/knex.js
+++ database/knex/knex.js
@@ -1,11 +1,13 @@
+require('dotenv').config()
+
 const knex = require('knex').knex({
     client: 'mysql',
     connection: {
         host: '127.0.0.1',
         port: 3306,
-        user: 'root',
-        password: 'admin1234',
-        database: 'flight_system'
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE
     }
 });
 
